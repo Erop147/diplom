53e49ce5a5cd3edf823029562520bd9bcf0df870
Egor Schelkonogov <egorshelk@gmail.com>
2013-12-10 02:27:33 +0600

    some command line parsing

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <pcap.h>
+#include <unistd.h>
+
 
 int PrintExistingDevices() {
     struct pcap_if* found_devices;
@@ -20,10 +22,35 @@ int PrintExistingDevices() {
     return 0;
 }
 
+void PrintHelp(char* progName) {
+    fprintf(stderr,
+        "USAGE: %s [-lh]\n"
+        "\n"
+        "-l    print list of suitable devices. Must be root\n"
+        "-h    print this help\n"
+        , progName
+    );
+}
 
 int main(int argc, char *argv[])
 {
-    PrintExistingDevices();
+    int c;
+    int hasArgs = 0;
+    while((c = getopt(argc, argv, "lh")) != -1) {
+        hasArgs = 1;
+        switch (c)
+        {
+        case 'l':
+            return PrintExistingDevices();
+        case 'h':
+            PrintHelp(argv[0]);
+            return 1;
+        }
+    }
+    if (!hasArgs) {
+        PrintHelp(argv[0]);
+        return 1;
+    }
     return 0;
     char *dev, errbuf[PCAP_ERRBUF_SIZE];
 
