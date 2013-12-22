cc040af5922b015760933f94365361f9b83a5e08
Egor Schelkonogov <egorshelk@gmail.com>
2013-12-22 18:50:38 +0600

    packet structs

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <pcap.h>
 #include <unistd.h>
+#include <stdint.h>
 
 int GetDefaultDevice(char** res) {
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -61,6 +62,42 @@ void PrintHelp(char* progName) {
     );
 }
 
+struct TEthernet {
+    uint8_t Dest[6];
+    uint8_t Source[6];
+    uint16_t Type;
+    uint8_t Payload[1500];
+};
+
+struct TIP {
+    uint8_t VersionAndLength;
+    uint8_t Flags;
+    uint16_t TotalLength;
+    uint16_t ID;
+    uint16_t FlagsAndOffset;
+    uint8_t TTL;
+    uint8_t Protocol;
+    uint16_t CheckSum;
+    uint8_t Source[4];
+    uint8_t Dest[4];
+    uint8_t Payload[1480];
+};
+
+struct TUDP {
+    uint16_t SourcePort;
+    uint16_t DestPort;
+    uint16_t Length;
+    uint16_t CheckSum;
+    uint8_t Payload[1472];
+};
+
+struct UDPPacket
+{
+    struct TEthernet Ethernet;
+    struct TIP* IP;
+    struct TUDP* UDP;
+};
+
 void SendTestTraffic(char* device) {
     /*if (device == NULL) {
         int res = GetDefaultDevice(device);
@@ -77,12 +114,21 @@ void SendTestTraffic(char* device) {
         if (packet == NULL)
             printf("null\n");
         else
+        {
             printf("len: %d\n", header.len);
+            int j;
+            for (j = 0; j < header.len; ++j)
+            {
+                printf("%c", packet[j]);
+            }
+            return;
+        }
     }
 }
 
 int main(int argc, char *argv[])
 {
+    printf("%d\n", sizeof(uint16_t));
     int c;
     int hasArgs = 0;
     char* device = NULL;
